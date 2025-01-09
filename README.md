3256421bbdf2e660c542b778fce7e7bf7d5d5ba5
vivek <rathorevivek050@gmail.com>
2025-01-09 16:52:48 +0530

    06

--- README.md
+++ README.md
@@ -13,3 +13,7 @@
 - Run Local and on Selenoid Integration
 
 
+<img width="1024" alt="Framework 1" src="https://github.com/user-attachments/assets/41bc1bb7-d128-495e-8621-51e685994a76" />
+
+
+![Framework 2](https://github.com/user-attachments/assets/e5e79c99-e4dd-4121-b3e9-4000498f84c8)
---
+++ src/main/java/com/thetestingacademy/pages/pageObjectModel/LoginPage_POM.java
@@ -0,0 +1,51 @@
+package com.thetestingacademy.pages.pageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage_POM {
+
+    WebDriver driver;
+
+    public LoginPage_POM(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    // Page Locators
+    private By username = By.id("login-username");
+    private By password = By.id("login-password");
+    private By signButton = By.id("js-login-btn");
+    private By error_message = By.id("js-notification-box-msg");
+
+    // If you are not using it , don't keep.
+
+
+    // Page Actions
+
+    public String loginToVWOLoginInvalidCreds(String user, String pwd) {
+
+        driver.findElement(username).sendKeys(user);
+        driver.findElement(password).sendKeys(pwd);
+        driver.findElement(signButton).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String error_message_text = driver.findElement(error_message).getText();
+        return error_message_text;
+
+    }
+
+    public void loginToVWOLoginValidCreds(String user, String pwd) {
+        driver.findElement(username).sendKeys(user);
+        driver.findElement(password).sendKeys(pwd);
+        driver.findElement(signButton).click();
+
+
+    }
+
+
+}
